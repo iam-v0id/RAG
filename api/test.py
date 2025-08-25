1a743227f5c34c110a8665a10a18bfd082eb571a
iam-v0id <korampallysuhaas@gmail.com>
2025-08-25 21:33:53 +0530

    Fix vercel congig

---
+++ api/test.py
@@ -0,0 +1,10 @@
+# Minimal test function for Vercel
+def handler(request):
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": '{"message": "Hello from Vercel Python!"}',
+    }
